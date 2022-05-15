715cd9633f1a3d2f854e2abac28229ccf9918c13
pradhumnts <62463488+pradhumnts@users.noreply.github.com>
2022-05-15 19:14:59 +0530

    Added auth

--- qbank/serializers.py
+++ qbank/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import *
-
+from django.contrib.auth.models import User
+from rest_framework_jwt.settings import api_settings
 
 class TopicSerializer(serializers.ModelSerializer):
     questions_length = serializers.SerializerMethodField("length_field")
@@ -69,3 +70,38 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = ["id", "courseName", "description",
                   "sections", "created_at", "updated_at"]
         depth = 4
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class UserSerializerWithToken(serializers.ModelSerializer):
+
+    token = serializers.SerializerMethodField()
+    password = serializers.CharField(write_only=True)
+
+    def get_token(self, obj):
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(obj)
+        token = jwt_encode_handler(payload)
+
+        return token
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = self.Meta.model(**validated_data)
+    
+        if password is not None:
+            user.set_password(password)
+            
+        user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = '__all__'
--- qbank/views.py
+++ qbank/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 
-from qbank.serializers import CourseSerializer, QuestionSerializer
+from qbank.serializers import CourseSerializer, QuestionSerializer, UserSerializer, UserSerializerWithToken
 from .models import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions, status
 
 # Create your views here.
 def index(request):
@@ -5235,3 +5237,44 @@ def questions_list(request):
         questions = Question.objects.all()
         serializer = QuestionSerializer(questions, many=True)
         return Response(serializer.data)
+
+@api_view(['GET'])
+def current_user(request):
+    print(request.user)
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+class UserList(APIView):
+    """
+    Create a new user. It's called 'UserList' because normally we'd have a get
+    method here too, for retrieving a list of all User objects.
+    """
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserCreate(APIView):
+    """
+    Create a new user. It's called 'UserList' because normally we'd have a get
+    method here too, for retrieving a list of all User objects.
+    """
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
